b599b727dfe448c663b179081fa0eb6ee84423e2
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2020-04-30 15:00:47 -0500

    add logic to push changes to github repo

--- src/commitWithGit.js
+++ src/commitWithGit.js
@@ -3,7 +3,7 @@ const git = require('simple-git');
 const makeACommit = async () => {
 
   // add a file to stage
-  const result = await new Promise((resolve, reject) => {
+  let result = await new Promise((resolve, reject) => {
     // git function receive a path param, that can be assumed as the 
     // current path if it is not provided
     git().add(['test.csv'], (error, data) => {
@@ -15,7 +15,31 @@ const makeACommit = async () => {
     });
   });
 
-  console.log(result);
+  console.log(result, 'stage file');
+
+  // commit the changes
+  result = await new Promise((resolve, reject) => {
+    git().commit('add changes automatically', (error, data) => {
+      if(error) {
+        reject(error);
+      }
+
+      resolve(data);
+    });
+  });
+
+  console.log(result, 'commit changes');
+
+  // push to repo
+  result = await new Promise((resolve, reject) => {
+    git().push('origin', 'master', (error, data) => {
+      if(error) {
+        reject(error);
+      }
+
+      resolve(data);
+    });
+  });
 }; 
 
 module.exports = {
